82d44fa8b7272f0100aa8bcccb8942d807fd3c0b
cc-santiago-alvarez <santiago.salinas@finoservices.com>
2025-06-25 15:58:11 -0500

    feat: se agregan los asteroides con movimiento aleatorio y colision

---
+++ asteroid.py
@@ -0,0 +1,18 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), (int(self.position.x), int(self.position.y)), self.radius, width=2)
+
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+        pass
+    
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,9 +13,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = updatable, drawable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     
     while True:
@@ -22,6 +29,12 @@ def main():
         
         updatable.update(dt)
         screen.fill((0, 0, 0))
+        
+        for asteroid in asteroids:
+            if player.collide(asteroid):
+                print("Game over!")
+                pygame.quit()
+                sys.exit()
         
         for obj in drawable:
             obj.draw(screen)
